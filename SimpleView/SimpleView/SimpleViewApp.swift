b64cbbd4796a021a5226f6034167eca381da6617
rmfls0606 <rmfls0606@naver.com>
2024-06-17 11:15:19 +0900

    SimpleView Project 생성

---
+++ SimpleView/SimpleView/SimpleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleViewApp.swift
+//  SimpleView
+//
+//  Created by 이상민 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
