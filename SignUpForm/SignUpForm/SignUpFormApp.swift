b9c3d7ab069fbbd0a8f6112bad5be5fe36d28154
rmfls0606 <rmfls0606@naver.com>
2024-06-18 16:05:30 +0900

    SignUpForm 프로젝트 생성

---
+++ SignUpForm/SignUpForm/SignUpFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFormApp.swift
+//  SignUpForm
+//
+//  Created by 이상민 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SignUpFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
