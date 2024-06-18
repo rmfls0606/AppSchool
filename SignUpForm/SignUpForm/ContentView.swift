2012bfe72d4389df6c5894093208850fc7bd8e4d
rmfls0606 <rmfls0606@naver.com>
2024-06-18 16:22:49 +0900

    username map

--- SignUpForm/SignUpForm/ContentView.swift
+++ SignUpForm/SignUpForm/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+class SignUpFormViewModel: ObservableObject{
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
+    
+    init(){
+        $username.map{ $0.count >= 3 }
+            .assign(to: &$isValid)
+        $username.map{ $0.count >= 3 ? "" : "Username must be at least three characters!" }
+            .assign(to: &$usernameMessage)
+    }
+}
+
+
 struct ContentView: View {
+    @StateObject var viewModel = SignUpFormViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form{
+            //username
+            Section(content: {
+                TextField("Username", text: $viewModel.username)
+                    .autocorrectionDisabled()
+            },footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundStyle(.red)
+            })
+            
+            //password
+            Section{
+                SecureField("Password", text: $viewModel.password)
+                SecureField("Password", text: $viewModel.passwordConfirmation)
+            }footer: {
+                Text(viewModel.passwordMessage)
+                    .foregroundStyle(.red)
+            }
+            //Submit
+            Section{
+                Button("Sign up"){
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
         }
-        .padding()
     }
 }
 
