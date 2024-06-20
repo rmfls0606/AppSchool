c82b8b110433e7dcb0b7555fba1ceee831177f35
rmfls0606 <rmfls0606@naver.com>
2024-06-20 11:12:38 +0900

    AuthenticationService url 추가

--- SignUpForm2/SignUpForm2/Helper.swift
+++ SignUpForm2/SignUpForm2/Helper.swift
@@ -25,7 +25,12 @@ enum APIError: LocalizedError{
 
 extension Publisher{
     func asResult() -> AnyPublisher<Result<Output, Failure>, Never>{
+        //Publisher의 모든 출력 값을 Result.success로 매핑합니다.
+        //즉, 성공적인 값을 Result<Output, Failure>.success 타입으로 래핑합니다.
+        //예를 들어, Output이 Int라면 Result.success(값)이 됩니다.
         self.map(Result.success)
+            //만약 Publisher가 오류를 발생시키면, 이 오류를 잡아서(catch) Result.failure로 변환합니다.
+            //Just(.failure(error))는 하나의 요소를 발행하는 Publisher를 반환하는데, 이 요소는 Result<Output, Failure>.failure 타입입니다.
             .catch{ error in
                 Just(.failure(error))
             }
