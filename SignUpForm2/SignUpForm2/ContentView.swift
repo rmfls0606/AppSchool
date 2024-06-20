6f5578af27ccc5af2e8019577550201069bcf278
rmfls0606 <rmfls0606@naver.com>
2024-06-20 09:26:55 +0900

    SignUpFormViewModel 및 View 정의

--- SignUpForm2/SignUpForm2/ContentView.swift
+++ SignUpForm2/SignUpForm2/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = SignUpFormViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form{
+            Section{
+                TextField("Username", text: $viewModel.username)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+            }footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundStyle(.red)
+            }
+            
+            Section{
+                Button("Sign up"){
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
         }
-        .padding()
+        .alert("Please update", isPresented: $viewModel.showUpdateDialog, actions: {
+            Button("Update"){
+                
+            }
+            
+            Button("Not now", role: .cancel){}
+        }, message: {
+            Text("It looks like you're using an older version of this app. Please updat your app.")
+        })
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack{
+        ContentView()
+    }
 }
